64199f886fc9c5f61c7f3aefba979df2d5eadb36
Gopal@02 <gopalchauhan.gc17@gmail.com>
2025-05-22 17:20:11 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from src.mlProject import logger
-from src.mlProject.utils.common import read_yaml
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is our custom log!")
 
+STAGE_NAME = "Data Ingestion stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_ingestion=DataIngestionTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -4,16 +4,16 @@ import yaml
 from src.mlProject import logger
 import json
 import joblib
-from ensure import ensure_annotiations
+from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
-@ensure_annotiations
+@ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
-       with open (path_to_yaml) as yaml_file:
-         content=yaml.safe_load(yaml_file)
+       with open(path_to_yaml) as yaml_file:
+         content = yaml.safe_load(yaml_file)
          logger.info(f"yaml file: {path_to_yaml} loaded successfully")
          return ConfigBox(content)
     except BoxValueError:
@@ -21,21 +21,21 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     except Exception as e:
        raise e
 
-@ensure_annotiations
+@ensure_annotations
 def create_directories(path_to_directories:list, verbsoe=True):
     for path in path_to_directories:
       os.makedirs(path,exist_ok=True) 
       if verbsoe:
         logger.info(f"created directory at: {path}")
 
-@ensure_annotiations
+@ensure_annotations
 def save_json(path:Path,date:dict):
     with open(path,"w") as f:
       json.dump(date,f,indent=4)
     
     logger.info(f"json file saved at : {path}")
 
-@ensure_annotiations
+@ensure_annotations
 def load_json(path:Path)-> ConfigBox:
   with open(path) as f:
     content = json.load(f)
@@ -43,18 +43,18 @@ def load_json(path:Path)-> ConfigBox:
   logger.info(f"json file loaded successfully from : {path}")
   return ConfigBox(content)
 
-@ensure_annotiations
+@ensure_annotations
 def save_bin(data: Any , path:Path):
   joblib.dump(value=data, filename=path)
   logger.info(f"binary file saved at:{path}")
 
-@ensure_annotiations
+@ensure_annotations
 def load_bin(path:Path)->Any:
   data = joblib.load(path)
   logger.info(f"binary file loaded from: {path}")
   return data
 
-@ensure_annotiations
+@ensure_annotations
 def get_size(path:Path)->str:
   size_in_kb=round(os.path.getsize(path)/1024)
   return f"~ {size_in_kb} kb"
