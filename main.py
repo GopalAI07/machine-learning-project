b12103e308e6145e78b88cd7b4633311c7b42e1f
Gopal@02 <gopalchauhan.gc17@gmail.com>
2025-05-23 11:33:19 +0530

    data transformation added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 if __name__ == '__main__':
@@ -24,3 +24,15 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e
+    
+STAGE_NAME = "Data Transformation stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_transformation = DataTransformationTrainingPipeline()
+        data_transformation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
